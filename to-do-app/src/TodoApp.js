22634ecd4b0979487251b18f95ce886666b06b75
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-09-11 02:44:41 -0300

    redux working

--- to-do-app/src/TodoApp.js
+++ to-do-app/src/TodoApp.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import TodoForm from './components/TodoForm'
+import rootReducer from './reducers'
+
+import devToolsEnhancer from 'remote-redux-devtools'
+
+const store = createStore(rootReducer, devToolsEnhancer())
 
 export default class TodoApp extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <TodoForm />
-            </View>
+            <Provider store={store}>
+                <View style={styles.container}>
+                    <TodoForm />
+                </View>
+            </Provider>
         )
     }
 }
